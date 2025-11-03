5d1915d48d73c749143952f51a2676035adfe1cb
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-03 23:46:36 +0530

    Hero Section complited

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 // jay dev baba
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <NavBar/>
       <Hero/>
+      <ProductViewer/>
     </div>
   )
 }
---
+++ src/components/ProductViewer.jsx
@@ -0,0 +1,29 @@
+
+
+function ProductViewer() {
+    return (
+        <section id="product-viewer">
+            <h2>Take a closer look.</h2>
+
+            <div className="controls">
+                <p className="info">MacbookPro 16" in Silver / Space Black
+                </p>
+                <div className="flex-center gap-5 mt-5">
+                    <div className="color-control">
+                        <div className="bg-neutral-300" />
+                        <div className="bg-neutral-900" />
+
+                    </div>
+                    <div className="size-control">
+                        <div><p>14"</p></div>
+                        <div><p>16"</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default ProductViewer
