9e21564f26ac5e6e7c3428f369a1ebb3b252716a
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-07 22:47:25 +0530

    change index.js useMacbookStore

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -9,13 +9,14 @@ function ProductViewer() {
             <h2>Take a closer look.</h2>
 
             <div className="controls">
-                <p className="info">MacbookPro 16" in Silver / Space Black
+                <p className="info">MacbookPro 16" in {color}
                 </p>
                 <div className="flex-center gap-5 mt-5">
                     <div className="color-control">
                         <div 
                         onClick={() => setColor('#adb5bd') } className={clsx('bg-neutral-300', color === '#abd5bd' && 'active')} />
-                        <div className="bg-neutral-900" />
+                        <div 
+                        onClick={() => setColor('#2e2c2e') } className={clsx('bg-neutral-300', color === '#2e2c2e' && 'active')} />
 
                     </div>
                     <div className="size-control">
