0507ae302dd429606fe668726a5b606c8e319275
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2025-11-08 20:40:41 +0530

    add react three fiber and drai

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -40,8 +40,9 @@ function ProductViewer() {
                 </div>
 
             </div>
-            <Canvas id="canvas">
-                <Box position={[-1,1,0]}></Box>
+            <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+                <Box position={[0,0,0]} scale={10 * scale} material-color = {color} />
+                < OrbitControls enableZoom={false} />
             </Canvas>
         </section>
     )
